8b85dc21c7b13fa9a5d610da92bfdaa59fb1e0d2
victor.arana <victor.arana@walmart.com>
2021-08-31 12:49:01 -0500

    :setup: initialize projecct using spring initializer
    Changes committed:
    	new file:   .gitignore
    	new file:   pom.xml
    	new file:   src/main/java/com/tribal/creditline/CreditLineApiApplication.java
    	new file:   src/main/resources/application.properties
    	new file:   src/test/java/com/tribal/creditline/CreditLineApiApplicationTests.java

---
+++ src/main/java/com/tribal/creditline/CreditLineApiApplication.java
@@ -0,0 +1,13 @@
+package com.tribal.creditline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditLineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditLineApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tribal/creditline/CreditLineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tribal.creditline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditLineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
